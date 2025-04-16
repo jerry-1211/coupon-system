b45e3cdedefddb7c4e9e2034b9384a9a31ce0ce8
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-16 16:31:40 +0900

    동시성 테스트 케이스 추가 - 멀티 스레드를 사용해서 테스트 진행 - 쿠폰은 100개이지만, 신청된 쿠폰의 개수 100개를 넘음 - Race Condition 문제 발생

--- api/src/test/java/com/example/api/Service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/Service/ApplyServiceTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -17,13 +22,39 @@ class ApplyServiceTest {
     private CouponRepository couponRepository;
 
     @Test
-    public void 한번만응모(){
+    public void 한번만응모() {
         applyService.apply(1L);
 
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1);
-
     }
 
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        /**
+         * Race Condition 때문에 100 제한을 걸어두었지만,초과해서 발행된다.
+         * */
+        assertThat(count).isNotEqualTo(100);
+    }
 }
